d457af861f4a6007aecd806f05bbdc2d446d9e32
Khaled Wahba <khaled.hassan1994@gmail.com>
2025-07-25 20:11:26 +0200

    update weights

--- src/optimization/payloadTransport_optimization.cpp
+++ src/optimization/payloadTransport_optimization.cpp
@@ -13,7 +13,7 @@ bool execute_payloadTransportOptimization(std::string &env_file,
                                     std::string &output_file_anytime,
                                     Trajectory &sol,
                                     const std::string &dynobench_base,
-                                    bool sum_robots_cost) {
+                                    bool sum_robots_cost, Trajectory &sol_broken) {
 
 
   Result_opti result;
@@ -21,13 +21,14 @@ bool execute_payloadTransportOptimization(std::string &env_file,
   Options_trajopt options_trajopt;
   options_trajopt.solver_id = 1;
   options_trajopt.collision_weight = 500.;
-  options_trajopt.weight_goal = 200.;
-  options_trajopt.max_iter = 50;
+  options_trajopt.weight_goal = 1000.;
+  options_trajopt.max_iter = 100;
   Problem problem(env_file);
   problem.models_base_path = dynobench_base + std::string("models/");
   Trajectory init_guess(initial_guess_file);
   trajectory_optimization(problem, init_guess, options_trajopt, sol, result);
-
+  sol_broken.states = result.xs_out;
+  sol_broken.actions = result.us_out;
   // sol.to_yaml_format(output_file);
   
   if (result.feasible) {
